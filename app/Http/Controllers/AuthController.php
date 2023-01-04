8376c0c8947c5c11909222154610c78b0383246b
Eliott HUBERT <e.hubert@eleve.leschartreux.net>
2023-01-04 16:59:11 +0100

    ajout fonctionalite

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
@@ -11,15 +12,15 @@ class AuthController extends Controller{
     public function login(Request $request){
 
         $users = User::all();
-
         foreach($users as $usertest){
             if($usertest->login == $request->login){
                 if($usertest->password == $request->password){
-                    return view(['afficheDossiers']);
+                    session_start();
+                    $_SESSION['user'] = $usertest;
+                    return redirect('listeDossiers');
             }
         }
-        return view(['login']);
-
     }
+    return view('login');
 }
 }
--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -1,4 +1,13 @@
 <div style="background-color: #3d3d3d; height: calc(100vh - 3.5rem); max-width: 15%; border-top-right-radius: 15px;">
-    <p>qsdsq</p>
+    <?php
+        use App\Models\User;
+        session_start();
+        $user = new User;
+        $user = $_SESSION['user'];
+        ?>
+        <div class="">
+            <p style="color: white">Utilisateur connecté: {{$user->login}}</p>
+        </div>
+
 </div>
 

4f1de499117193ff0302ec9a4b0453159000bb75
Nils Oberholz <n.oberholz@eleve.leschartreux.net>
2023-01-09 13:32:13 +0100

    patch panel

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -3,13 +3,13 @@
             <p style="color: white">Utilisateur connecté: </p>
         </div>
         <div class="text-center" style="margin-bottom: 2%">
-            <button type="button" class="btn btn-secondary" onclick="location.href='listeDossiers'">Mes dossiers<img src="../../../../Images/folder.svg" style=""></button>
+            <button type="button" class="btn btn-secondary" onclick="location.href='/listeDossiers'">Mes dossiers<img src="../../../../Images/folder.svg" style=""></button>
         </div>
         <div class="text-center" style="margin-bottom: 2%">
             <button type="button" class="btn btn-secondary" onclick="location.href='#'">Mes demandes<img src="../../../../Images/folder.svg" style=""></button>
         </div>
         <div class="text-center">
-            <button type="button" class="btn btn-danger" onclick="location.href='logout'">Déconnexion<img src="../../../../Images/logout.svg" style=""></button>
+            <button type="button" class="btn btn-danger" onclick="location.href='/logout'">Déconnexion<img src="../../../../Images/logout.svg" style=""></button>
         </div>
 
 </div>
